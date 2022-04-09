cea24d2637cacea38fb0c1ca90f40141b58a31dc
Vaishakbg <vaishakbg@gmail.com>
2022-04-09 21:35:33 +0530

    Proper List using map

--- src/index.js
+++ src/index.js
@@ -2,40 +2,43 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL604_SR604,400_.jpg",
+    title: "It's Not Easy Being a Bunny (Beginner Books(R))",
+    author: "Marilyn Sadler",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg",
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    author: "James Clear",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81DSi4ECwyL._AC_UL604_SR604,400_.jpg",
+    title:
+      "The Mothers and Daughters of the Bible Speak: Lessons on Faith from Nine Biblical Families",
+    author: "Shannon Bream",
+  },
+];
 
 function BookList() {
   return (
     <section className="book-list">
-      <Book
-        image="https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL604_SR604,400_.jpg"
-        title="It's Not Easy Being a Bunny (Beginner Books(R))"
-        author="Marilyn Sadler"
-      >
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-          inventore vitae fugit soluta ex alias, perspiciatis quasi sint id nam
-          dolorem. Deleniti delectus hic blanditiis consequuntur incidunt
-          officia dolorum eum?
-        </p>
-      </Book>
-      <Book
-        image="https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg"
-        title="Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones"
-        author="James Clear"
-      />
+      {books.map((book) => {
+        return <Book book={book} />;
+      })}
     </section>
   );
 }
 
-const Book = ({ image, title, author, children }) => {
-  // const Book = (props) => {
-  // const { image, title, author } = props;
+const Book = (props) => {
+  const { img, title, author } = props.book;
   return (
     <article className="book">
-      <img src={image} alt="book" width={200} />
+      <img src={img} alt="book" width={200} />
       <h2 className="book-title">{title}</h2>
-      <p className="author">{author.toUpperCase()}</p>
-      {children}
+      <span className="author">{author.toUpperCase()}</span>
     </article>
   );
 };
