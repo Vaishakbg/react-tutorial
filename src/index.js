24fa4f3a9e2dd1822f028de6f0f3a1150d97a544
Vaishakbg <vaishakbg@gmail.com>
2022-04-09 20:59:56 +0530

    remove default fils

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className="book-list">
+      <Book
+        image="https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL604_SR604,400_.jpg"
+        title="It's Not Easy Being a Bunny (Beginner Books(R))"
+        author="Marilyn Sadler"
+      />
+      <Book
+        image="https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg"
+        title="Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones"
+        author="James Clear"
+      />
+    </section>
+  );
+}
+
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={props.image} alt="book" width={200} />
+      <h2 className="book-title">{props.title}</h2>
+      <p className="author">{props.author.toUpperCase()}</p>
+      <span>{2 * 45}</span>
+    </article>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<BookList />);
