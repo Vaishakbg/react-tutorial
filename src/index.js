e0e4cd16b627abc6411241f766d1b8c4669ac0e7
Vaishakbg <vaishakbg@gmail.com>
2022-04-09 21:37:27 +0530

    removing Key error

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 const books = [
   {
+    id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL604_SR604,400_.jpg",
     title: "It's Not Easy Being a Bunny (Beginner Books(R))",
     author: "Marilyn Sadler",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg",
     title:
       "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
     author: "James Clear",
   },
   {
+    id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/81DSi4ECwyL._AC_UL604_SR604,400_.jpg",
     title:
       "The Mothers and Daughters of the Bible Speak: Lessons on Faith from Nine Biblical Families",
@@ -26,7 +29,7 @@ function BookList() {
   return (
     <section className="book-list">
       {books.map((book) => {
-        return <Book book={book} />;
+        return <Book key={book.id} book={book} />;
       })}
     </section>
   );
@@ -36,7 +39,7 @@ const Book = (props) => {
   const { img, title, author } = props.book;
   return (
     <article className="book">
-      <img src={img} alt="book" width={200} />
+      <img src={img} alt="book" />
       <h2 className="book-title">{title}</h2>
       <span className="author">{author.toUpperCase()}</span>
     </article>
