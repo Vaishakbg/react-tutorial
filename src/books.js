ef2d33d0c83f405d485d6db87ecd10d1a883b489
Vaishakbg <vaishakbg@gmail.com>
2022-04-11 20:11:34 +0530

    untraced

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL604_SR604,400_.jpg",
+    title: "It's Not Easy Being a Bunny",
+    author: "Marilyn Sadler",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81DSi4ECwyL._AC_UL604_SR604,400_.jpg",
+    title: "The Mothers and Daughters of the Bible Speak",
+    author: "Shannon Bream",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import { books } from "./books";
+
 import "./index.css";
-const books = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL604_SR604,400_.jpg",
-    title: "It's Not Easy Being a Bunny",
-    author: "Marilyn Sadler",
-  },
-  {
-    id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg",
-    title: "Atomic Habits",
-    author: "James Clear",
-  },
-  {
-    id: 3,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81DSi4ECwyL._AC_UL604_SR604,400_.jpg",
-    title: "The Mothers and Daughters of the Bible Speak",
-    author: "Shannon Bream",
-  },
-];
 
 function BookList() {
   return (
