70009f8ac23397612e5bb08eae91058dfe62dad1
Vaishakbg <vaishakbg@gmail.com>
2022-04-12 16:37:35 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Book from "./Book";
+import { Greeting } from "./testing/testing";
 
 import { books } from "./books";
 
 import "./index.css";
 
 function BookList() {
+  console.log(Greeting);
   return (
     <section className="book-list">
       {/* {books.map((book) => {
@@ -17,40 +20,6 @@ function BookList() {
     </section>
   );
 }
-
-const Book = (props) => {
-  // attribute, eventHandler
-  // onclick, OnMouseOver
-  const clickHandler = (e) => {
-    console.log(e.target);
-  };
-
-  const complexExample = (author) => {
-    console.log(author);
-  };
-
-  const { img, title, author } = props;
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(author);
-      }}
-    >
-      <img src={img} alt="book" />
-      <h2 className="book-title">{title}</h2>
-      <span className="author" style={{ display: "block" }}>
-        {author.toUpperCase()}
-      </span>
-      <button type="button" onClick={clickHandler}>
-        button
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        Complex
-      </button>
-    </article>
-  );
-};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
