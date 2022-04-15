3da5e7c618d5773d43b75a375a2c1055e0e11af2
Vaishakbg <vaishakbg@gmail.com>
2022-04-15 20:30:43 +0530

    usestate-functional update Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import UseStateCounter from "./tutorial/1-useState/setup/5-useState-counter";
+import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
 function App() {
   return (
     <div className="container">
-      <UseStateCounter />
+      <UseStateArray />
     </div>
   );
 }
