1870c0b35367b325814c257ea7b062ec2ef23065
Vaishakbg <vaishakbg@gmail.com>
2022-04-25 18:21:47 +0530

    6-useReducer --- add item

--- src/tutorial/6-useReducer/index.js
+++ src/tutorial/6-useReducer/index.js
@@ -1,27 +1,52 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../data";
+
 // reducer function
+const reducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "item added!",
+    };
+  }
+  if (action.type === "NO_VALUE") {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: "please enter value",
+    };
+  }
+  throw new Error("no matching action type");
+};
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
 
 const Index = () => {
   const [name, setName] = useState("");
-  const [people, setPeople] = useState(data);
-  const [isShowModal, setIsShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setIsShowModal(true);
-      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
       setName("");
     } else {
-      setIsShowModal(true);
+      dispatch({ type: "NO_VALUE" });
     }
   };
 
   return (
     <>
-      {isShowModal && <Modal />}
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -32,7 +57,7 @@ const Index = () => {
         </div>
         <button type="submit">add</button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
           <div key={person.id}>
             <h4>{person.name}</h4>
